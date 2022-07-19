8c27b95721422732519480062d76b917b2f5e51f
Rogério Ferreira de Souza <rogeriofrsouza@gmail.com>
2022-07-19 18:15:02 -0300

    Implementação animação em banner component

--- src/app/acesso/banner/banner.component.ts
+++ src/app/acesso/banner/banner.component.ts
@@ -1,15 +1,56 @@
+import { animate, state, style, transition, trigger } from '@angular/animations';
 import { Component, OnInit } from '@angular/core';
+
+import { Imagem } from '../../shared/models/imagem.model';
 
 @Component({
   selector: 'app-banner',
   templateUrl: './banner.component.html',
-  styleUrls: ['./banner.component.css']
+  styleUrls: ['./banner.component.css'],
+  animations: [
+    trigger('banner', [
+      state('escondido', style({
+        opacity: 0
+      })),
+      state('visivel', style({
+        opacity: 1
+      })),
+      transition('escondido <=> visivel', animate('1s ease-in')),
+    ])
+  ]
 })
 export class BannerComponent implements OnInit {
+
+  public estado: string = 'visivel';
+
+  public imagens: Imagem[] = [
+    { estado: 'visivel', url: '../../../assets/banner-acesso/img_1.png' },
+    { estado: 'escondido', url: '../../../assets/banner-acesso/img_2.png' },
+    { estado: 'escondido', url: '../../../assets/banner-acesso/img_3.png' },
+    { estado: 'escondido', url: '../../../assets/banner-acesso/img_4.png' },
+    { estado: 'escondido', url: '../../../assets/banner-acesso/img_5.png' },
+  ];
 
   constructor() { }
 
   ngOnInit(): void {
+    setTimeout(() => this.logicaRotacao(), 4000);
+  }
+
+  public logicaRotacao(): void {
+    let idx: number = 0;
+
+    for(let i: number = 0; i <= 4; i++) {
+      if (this.imagens[i].estado === 'visivel') {
+        this.imagens[i].estado = 'escondido';
+        idx = i === 4 ? 0 : i + 1;
+
+        break;
+      }
+    }
+    this.imagens[idx].estado = 'visivel';
+
+    setTimeout(() => this.logicaRotacao(), 4000);
   }
 
 }
---
+++ src/app/shared/models/imagem.model.ts
@@ -0,0 +1,5 @@
+export interface Imagem {
+  estado: string;
+  url: string;
+  alt?: string;
+}
