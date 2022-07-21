3a4231f0cee4b8e9b6a911c0d9e5f77a3f72a934
Rogério Ferreira de Souza <rogeriofrsouza@gmail.com>
2022-07-21 13:24:07 -0300

    Animação banner e painel

--- src/app/acesso/acesso.component.ts
+++ src/app/acesso/acesso.component.ts
@@ -1,11 +1,31 @@
+import { trigger, state, style, transition, animate } from '@angular/animations';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-acesso',
   templateUrl: './acesso.component.html',
-  styleUrls: ['./acesso.component.css']
+  styleUrls: ['./acesso.component.css'],
+  animations: [
+    trigger('animacao-banner', [
+      state('criado', style({ opacity: 1 })),
+      transition('void => criado', [ 
+        style({ opacity: 0, transform: 'translate(-32px, 0)' }), //Estilo do estado inicial 'void'
+        animate('500ms 0s ease-in-out') //Duração, delay e aceleração
+      ])
+    ]),
+    trigger('animacao-painel', [
+      state('criado', style({ opacity: 1 })),
+      transition('void => criado', [
+        style({ opacity: 0, transform: 'translate(32px, 0)' }),
+        animate('500ms 0s ease-in-out')
+      ])
+    ])
+  ]
 })
 export class AcessoComponent implements OnInit {
+
+  public estadoBanner: string = 'criado';
+  public estadoPainel: string = 'criado';
 
   constructor() { }
 
